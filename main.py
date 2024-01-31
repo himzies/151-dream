02d3d02982a0e4cbe9a9561021b36b6dd2002722
himzies <himzies123@gmail.com>
2024-01-31 17:33:41 +0800

    update main and src codes

--- main.py
+++ main.py
@@ -2,22 +2,23 @@ import os
 
 from datetime import date
 from src.card_info import get_card_info, get_price_stat
-from src.constants import Constants
+# from src.constants import Constants
 
 if __name__ == '__main__':
     function = input('what would you like to do today? (pc - price check/pt - price track): ')
     game = input('what game? (ws - weiss schwarz/poc - pokemon): ')
     set = input('which set does it belong to? ')
     today = date.today()
-    filepath = f'data/{str(today)/{game}/{set}.csv}'
+    if not os.path.exists(f'data/{game}/{str(today)}'):
+        os.makedirs(f'data/{game}/{str(today)}')
+    filepath = f'data/{game}/{str(today)}/{set}.csv'
     if function == 'pt':
         if os.path.isfile(filepath):
             print(f'price track already done for {set} today')
         else:
-            os.makedirs(filepath)
             get_card_info(game, set).to_csv(filepath)
     elif function == 'pc':
         if os.path.isfile(filepath):
-            card_name = input('what is the name of the card? ')
-            price_stat = get_price_stat(card_name, filepath)
-            print(f'{card_name} costs {price_stat[0]}, {price_stat[1]}')
+            card_num = input('what is the number at the bottom of the card? ')
+            price_stat = get_price_stat(game, set, card_num, filepath)
+            print(f'{price_stat[0]} ({price_stat[1]}) costs {price_stat[2]}, {price_stat[3]}')
--- src/card_info.py
+++ src/card_info.py
@@ -1,9 +1,10 @@
+import os
 import pandas as pd
 import requests
 import re
 
 from bs4 import BeautifulSoup
-from src.constants import Constants
+# from src.constants import Constants
 
 def get_card_info(game, set):
     page = requests.get(f'https://yuyu-tei.jp/sell/{game}/s/{set}')
@@ -15,6 +16,7 @@ def get_card_info(game, set):
     card_nps = {}
     for card_info in all_card_info:
         card_name = card_info.find('h4').text.strip()
+        card_num = card_info.find('span').text.strip()
         card_price = card_info.find('strong').text.strip()
         card_status = card_info.find('label').text
         card_status = card_status.replace('\n', '')
@@ -22,6 +24,12 @@ def get_card_info(game, set):
         card_status = card_status.replace('在庫', 'remaining')
         card_status = card_status.replace('点', 'pcs')
         card_nps['card_name'] = card_name
+        card_nps['card_num'] = card_num
         card_nps['price'] = card_price
         card_nps['status'] = card_status
+
     return pd.DataFrame(card_nps)
+
+
+def get_price_stat(game, set, card_num, filepath):
+    if not os.path.isfile(filepath):
