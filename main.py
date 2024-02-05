f983de43973330713c8b8cfd73aefe3b9959f656
himzies <himzies123@gmail.com>
2024-02-05 15:55:38 +0800

    full update
    everything is ready, just need to find a way to translate the japanese booster names to english...

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import os
 
 from datetime import date
-from src.card_info import get_card_info, get_price_stat
+from src.card_info import *
 # from src.constants import Constants
 
 if __name__ == '__main__':
-    function = input('what would you like to do today? (pc - price check/pt - price track): ')
-    game = input('what game? (ws - weiss schwarz/poc - pokemon): ')
-    set = input('which set does it belong to? ')
     today = date.today()
-    if not os.path.exists(f'data/{game}/{str(today)}'):
-        os.makedirs(f'data/{game}/{str(today)}')
-    filepath = f'data/{game}/{str(today)}/{set}.csv'
-    if function == 'pt':
-        if os.path.isfile(filepath):
-            print(f'price track already done for {set} today')
-        else:
-            get_card_info(game, set).to_csv(filepath)
-    elif function == 'pc':
-        card_num = str(input('what is the number at the bottom of the card? '))
-        if not os.path.isfile(filepath):
-            get_card_info(game, set).to_csv(filepath)
-        price_stat = get_price_stat(set, card_num, filepath)
+    again = True
+    get_pkmn_links(today)
+    get_ws_links(today)
+    while again:
+        game = input('what game? (ws - weiss schwarz/poc - pokemon): ').lower()
+        get_set_names(game, today)
+        set = input('which set does it belong to? ').lower()
+        if game == 'ws':
+            set = input('which set does it belong to? ').lower() + '#kana'
+        card_num = str(input('what is the number at the bottom of the card? ')).upper()
+        link = link_retrieval(game, set, today)
+        set_info = get_set_info(game, set, today)
+        price_stat = get_price_stat(game, set, card_num, today)
         print(f'{price_stat[1]} ({price_stat[2]}) costs {price_stat[3]}, {price_stat[4]}')
+        prompt = input('wwould you like to search for another card? (y - yes/n - no): ').lower()
+        if prompt == 'n':
+            again = False
--- src/card_info.py
+++ src/card_info.py
@@ -4,18 +4,21 @@ import requests
 import re
 
 from bs4 import BeautifulSoup
+from datetime import date
 # from src.constants import Constants
 
-def get_card_info(game, set):
+def get_set_info(game, set, date):
+
     page = requests.get(f'https://yuyu-tei.jp/sell/{game}/s/{set}')
 
     soup = BeautifulSoup(page.content, 'html.parser')
 
     all_card_info = soup.findAll('div', attrs={'class': re.compile('card-product position-relative mt-4*')})
 
-    card_nps = {'card_name': [], 'card_num': [], 'price': [], 'status': []}
+    result = pd.DataFrame({'card_name': [], 'card_num': [], 'price': [], 'status': []})
+
     for card_info in all_card_info:
-        card_num = card_info.find('span').text.strip()
+        card_num = card_info.find('span').text.strip().upper()
         card_name = card_info.find('h4').text.strip()
         card_price = card_info.find('strong').text.strip()
         card_status = card_info.find('label').text
@@ -23,23 +26,24 @@ def get_card_info(game, set):
         card_status = card_status.replace(' ', '')
         card_status = card_status.replace('在庫', 'remaining')
         card_status = card_status.replace('点', 'pcs')
-        card_nps['card_num'].append(card_num)
-        card_nps['card_name'].append(card_name)
-        card_nps['price'].append(card_price)
-        card_nps['status'].append(card_status)
+        new_row = pd.DataFrame({'card_name': [card_name], 'card_num': [card_num], 'price': [card_price], 'status': [card_status]})
+        result = pd.concat([result, new_row])
     
-    result = pd.DataFrame.from_dict(card_nps)
+    result.to_csv(f'data/{game}/{str(date)}/{set}.csv')
 
     return result
 
 
-def get_price_stat(set, card_num, filepath):
-    data = pd.read_csv(filepath)
-    print(card_num)
+def get_price_stat(game, set, card_num, date):
+
+    data = pd.read_csv(f'data/{game}/{str(date)}/{set}.csv')
+
     card = data[data['card_num'] == card_num]
+
     if set == 'sv02a' and len(card.index) > 1:
         option = input('which version? (n - normal/pb - pokeball/mb - masterball) ')
         card = dream_function(card, option)
+
     return card.values.flatten().tolist()
 
 
@@ -52,40 +56,72 @@ def dream_function(card, option):
         return card.iloc[[2]]
     
 
-def get_set_links(game):
+def get_pkmn_links(date):
 
-    result = {}
+    if not os.path.exists(f'data/poc/{str(date)}'):
+        os.makedirs(f'data/poc/{str(date)}')
 
-    if game == 'poc':
+    result = pd.DataFrame({'set_code': [], 'set_name': [], 'set_link': []})
 
-        page = requests.get(f'https://yuyu-tei.jp/top/poc')
+    page = requests.get(f'https://yuyu-tei.jp/top/poc')
 
-        soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-        set_sections = soup.find('div', attrs={'id': 'side-sell-target-12'})
+    set_sections = soup.find('div', attrs={'id': 'side-sell-target-12'})
 
-        all_links = set_sections.findAll('button', attrs={'id': re.compile(f'side-sell-*')})
+    all_links = set_sections.findAll('button', attrs={'id': re.compile(f'side-sell-*')})
 
-        for links in all_links:
-            link = links['onclick'].split("'")[1]
-            set_name = links.text.replace('\n', '').lstrip()
-            set_code = link.replace('https://yuyu-tei.jp/sell/poc/s/', '')
-            result[set_code] = {'set_name': set_name, 'link': link}
+    for links in all_links:
+        link = links['onclick'].split("'")[1].strip()
+        set_name = links.text.strip()
+        set_code = link.replace('https://yuyu-tei.jp/sell/poc/s/', '')
+        new_row = pd.DataFrame({'set_code': [set_code], 'set_name': [set_name], 'set_link': [link]})
+        result = pd.concat([result, new_row])
+    
+    result = result.reset_index()
 
-    elif game == 'ws':
-
-        page = requests.get(f'https://yuyu-tei.jp/top/ws')
-
-        soup = BeautifulSoup(page.content, 'html.parser')
-
-        set_sections = soup.find('div', attrs={'id': 'side-sell-target-12'})
-
-        all_links = set_sections.findAll('a', attrs={'id': re.compile(f'side-sell-*')})
-
-        for links in all_links:
-            link = links['href']
-            set_name = links.text.replace('\n', '').lstrip()
-            set_code = link.replace('https://yuyu-tei.jp/sell/ws/s/', '')
-            result[set_code] = {'set_name': set_name, 'link': link}
+    result.to_csv(f'data/poc/{str(date)}/poc_links.csv')
     
     return result
+
+
+def get_ws_links(date):
+
+    if not os.path.exists(f'data/ws/{str(date)}'):
+        os.makedirs(f'data/ws/{str(date)}')
+
+    result = pd.DataFrame({'set_code': [], 'set_name': [], 'set_link': []})
+
+    page = requests.get(f'https://yuyu-tei.jp/top/ws')
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    set_sections = soup.find('div', attrs={'id': 'side-sell-target-12'})
+
+    all_links = set_sections.findAll('a', attrs={'id': re.compile(f'side-sell-*')})
+
+    for links in all_links:
+        link = links['href'].strip()
+        set_name = links.text.strip()
+        set_code = link.replace('https://yuyu-tei.jp/sell/ws/s/', '').replace('#kana', '')
+        new_row = pd.DataFrame({'set_code': [set_code], 'set_name': [set_name], 'set_link': [link]})
+        result = pd.concat([result, new_row])
+
+    result = result.reset_index()
+    
+    result.to_csv(f'data/ws/{str(date)}/ws_links.csv')
+    
+    return result
+
+
+def link_retrieval(game, set, date):
+    filepath = f'data/{game}/{str(date)}/{game}_links.csv'
+    df = pd.read_csv(filepath)
+    target = df[df['set_code'] == set]
+    return target['set_link']
+
+
+def get_set_names(game, date):
+    filepath = f'data/{game}/{str(date)}/{game}_links.csv'
+    df = pd.read_csv(filepath)
+    print(df[['set_code', 'set_name']].to_markdown())
